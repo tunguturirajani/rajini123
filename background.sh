22844b7f22fd4efb11419439e5109e04a0b29111
tunguturirajani <tunguturi.rajini434@gmail.com>
2021-04-07 07:39:50 +0000

    new file added

---
+++ background.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt update -y 
+apt install figlet toilet -y
+figlet "n C o d e IT . com" 
+toilet -f term -F border --gay "Welcoe to ncodeit.com custom server" ;
+echo "Welcome to nCodeIT custom Ubuntu Server"
